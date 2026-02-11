e7ab9f9921e887c674f0bc711b31dd5bc7e5ecba
Michael Grieco <michael.grieco@columbia.edu>
2026-02-11 10:21:29 -0500

    Update for annotation-style tracing with TraceDoctor

--- src/main/scala/TraceDoctor.scala
+++ src/main/scala/TraceDoctor.scala
@@ -1,6 +1,5 @@
 package testchipip
 
-//import chipsalliance.rocketchip.config.Field
 import org.chipsalliance.cde.config.{Field, Parameters}
 import chisel3._
 import org.chipsalliance.cde.config._
@@ -9,7 +8,6 @@ import freechips.rocketchip.diplomacy.LazyRawModuleImp
 import org.chipsalliance.diplomacy.bundlebridge.BundleBridgeNexusNode
 import freechips.rocketchip.rocket.TraceDoctor
 import freechips.rocketchip.subsystem.{BaseSubsystem, InstantiatesHierarchicalElements, HasHierarchicalElementsRootContext}
-//import freechips.rocketchip.util.HeterogeneousBag
 import org.chipsalliance.diplomacy.nodes.HeterogeneousBag
 
 // A per-tile interface that includes the tile's clock and reset
@@ -18,8 +16,7 @@ class TileTraceDoctorIO(_traceDoctorType: TraceDoctor) extends Bundle {
   val reset: Bool = Bool()
   val bits = Vec(traceWidth, Bool())
   val valid: Bool = Bool()
-  //val trace: TraceDoctor = new TraceDoctor(traceWidth)
-  val tracerVTrigger: Bool = Bool()
+  //val tracerVTrigger: Bool = Bool()
   val trace = traceType.cloneType
   def traceType = _traceDoctorType
   def traceWidth = traceType.traceWidth
@@ -28,10 +25,6 @@ class TileTraceDoctorIO(_traceDoctorType: TraceDoctor) extends Bundle {
 // The IO matched on by the TraceDoctor bridge: a wrapper around a heterogenous
 // bag of TileTraceDoctorIO. Each entry is trace associated with a single tile
 
-// must extend chipyard.iobinders.Port
-//class TraceOutputTop(coreTraces: Seq[TraceBundle]) extends Bundle {
-//  val traces = Output(HeterogeneousBag.apply(coreTraces.map(t => new TileTraceIO(t))))
-//}
 class TraceDoctorOutputTop(traces: Seq[TraceDoctor]) extends Bundle {
   val tracedoctors = Output(org.chipsalliance.diplomacy.nodes.HeterogeneousBag.apply(traces.map(t => new TileTraceDoctorIO(t))))
   val traceWidths = traces.map(t => t.traceWidth)
@@ -45,68 +38,13 @@ object TraceDoctorOutputTop {
     }))
 }
 
-// Use this trait:
-//trait CanHaveTraceDoctorIO { this: InstantiatesHierarchicalElements =>
-//trait CanHaveTraceDoctorIO { this: BaseSubsystem =>
-//  implicit val p: Parameters
-//
-//  val module: CanHaveTraceDoctorIOModuleImp
-//  // Bind all the trace nodes to a BB; we'll use this to generate the IO in the imp
-//  val traceDoctorNexus = BundleBridgeNexusNode[TraceDoctor]()
-//  //totalTiles.foreach { traceDoctorNexus := _._2.traceDoctorNode }
-//  val chipyardSystem = module.asInstanceOf[InstantiatesHierarchicalElements]
-//  //val tiles = chipyardSystem.totalTiles.values
-//  chipyardSystem.totalTiles.foreach { traceDoctorNexus := _._2.traceDoctorNode }
-//}
 case class TraceDoctorPortParams(print: Boolean = false)
 object TraceDoctorPortKey extends Field[Option[TraceDoctorPortParams]](None)
-
-//trait HasExtInterruptsModuleImp extends LazyRawModuleImp with HasExtInterruptsBundle {
-//  val outer: HasExtInterrupts
-//  val interrupts = IO(Input(UInt(outer.nExtInterrupts.W)))
-//
-//  outer.extInterrupts.out.map(_._1).flatten.zipWithIndex.foreach { case(o, i) => o := interrupts(i) }
-//}
-
-//trait CanHaveTraceDoctorIOModuleImp extends LazyModuleImp with HasTraceDoctorBundle {
-//trait CanHaveTraceDoctorIOModuleImp extends LazyRawModuleImp {
-//  //val outer: CanHaveTraceDoctorIO with InstantiatesHierarchicalElements
-//  val outer: CanHaveTraceDoctorIO
-//
-//  val traceDoctorIO = p(TraceDoctorPortKey) map ( traceParams => {
-//    val traceDoctorSeq = (outer.traceDoctorNexus.in.map(_._1))
-//    val tdio = IO(Output(TraceDoctorOutputTop(traceDoctorSeq)))
-//
-//    (tdio.tracedoctors zip (outer.tile_prci_domains zip traceDoctorSeq)).foreach { case (port, (prci, tracedoc)) =>
-//      port.clock := prci._2.module.clock
-//      port.reset := prci._2.module.reset.asBool
-//      port.data := tracedoc
-//
-//      port.tracerVTrigger := false.B
-//      midas.targetutils.TriggerSink.whenEnabled(false.B) {
-//        port.tracerVTrigger := true.B
-//      }
-//    }
-//
-//    if (traceParams.print) {
-//      for ((trace, idx) <- tdio.tracedoctors.zipWithIndex ) {
-//        withClockAndReset(trace.clock, trace.reset) {
-//          when (trace.data.valid) {
-//            printf(s"TraceDoctor $idx: %x\n", trace.data.bits.asUInt)
-//          }
-//        }
-//      }
-//    }
-//    tdio
-//  })
-//}
-
 
 trait CanHaveTraceDoctorIO { this: HasHierarchicalElementsRootContext with InstantiatesHierarchicalElements =>
   implicit val p: Parameters
 
   val tileTraceDoctorNodes = traceDoctorNodes.values
-  // val tileTraceNodes = traceNodes.values // TODO FIND where traceNodes and traceDoctorNodes are!
 
   val traceDoctorIO = InModuleBody { p(TraceDoctorPortKey) map ( traceParams => {
     println(s"MG tileTraceDoctorNodes length is ${tileTraceDoctorNodes.size}")
@@ -121,9 +59,8 @@ trait CanHaveTraceDoctorIO { this: HasHierarchicalElementsRootContext with Insta
       port.trace := tracedoc.bundle
       port.valid := tracedoc.bundle.valid
       port.bits := tracedoc.bundle.bits
-      //port.bits := tracedoc.bundle.bits
 
-      port.tracerVTrigger := false.B
+      //port.tracerVTrigger := false.B
       //midas.targetutils.TriggerSink.whenEnabled(false.B) {
       //  port.tracerVTrigger := true.B
       //}
